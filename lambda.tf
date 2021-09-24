75f7f7b0163e6c22dc57dbb28939e164c5455357
jmanzur <jmanzurst@gmail.com>
2021-09-24 10:51:04 -0400

    Adding tags to lambda

--- lambda.tf
+++ lambda.tf
@@ -11,6 +11,7 @@ resource "aws_lambda_function" "startstop_ec2_function" {
   function_name = "startstop_ec2_function"
   role          = aws_iam_role.startstop_ec2_policy_role.arn
   handler       = "startstop_ec2.lambda_handler"
+  tags          = merge(var.demo-startstop, { Name = "${var.tag_project}-lambda" })
 
   # Prevent lambda recreation
   source_code_hash = filebase64sha256(data.archive_file.init.output_path)
