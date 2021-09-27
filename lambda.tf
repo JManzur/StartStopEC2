5af7e40c0bc7853c99b4d20e641e7dbe3f63f867
jmanzur <jmanzurst@gmail.com>
2021-09-27 13:11:02 -0400

    Adding Lambda description

--- lambda.tf
+++ lambda.tf
@@ -11,10 +11,11 @@ resource "aws_lambda_function" "startstop_ec2_function" {
   function_name = "startstop_ec2_function"
   role          = aws_iam_role.startstop_ec2_policy_role.arn
   handler       = "startstop_ec2.lambda_handler"
+  description   = "Start/Stop EC2 on a schedule"
   tags          = merge(var.demo-startstop, { Name = "${var.tag_project}-lambda" })
 
   # Prevent lambda recreation
   source_code_hash = filebase64sha256(data.archive_file.init.output_path)
 
-  runtime = "python3.8"
+  runtime = "python3.9"
 }
